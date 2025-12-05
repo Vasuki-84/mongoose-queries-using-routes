6ae5b8a017d1f3f7743427a160b32bba4b87e9c6
Vasuki t <iamvasukithangaraj@gmail.com>
2025-12-05 21:46:12 +0530

    added mongoDB compass

--- config/dbConnection.config.js
+++ config/dbConnection.config.js
@@ -1,15 +1,24 @@
 const mongoose = require("mongoose");
-const dbconnection = require("../../../../../MERN_Entri/Backend/mongoose/config/dbconnection.config");
 
-const dbAtlas = async () => {
+const dbconnection = async () => {
   try {
-    await mongoose.connect(process.env.dbport, {
-      ssl: true,
-      tlsAllowInvalidCertificates: false,
-    });
-    console.log("database Atlas connected succcesfully");
+    await mongoose.connect(process.env.dbport);
+    console.log("connection success");
   } catch (err) {
-    console.log("database Atlas connection failed");
+    console.log("connection failed");
   }
 };
-module.exports= dbconnection;
+module.exports = dbconnection;
+
+// const dbAtlas = async () => {
+//   try {
+//     await mongoose.connect(process.env.dbport, {
+//       ssl: true,
+//       tlsAllowInvalidCertificates: false,
+//     });
+//     console.log("database Atlas connected succcesfully");
+//   } catch (err) {
+//     console.log("database Atlas connection failed");
+//   }
+// };
+// module.exports= dbconnection;
