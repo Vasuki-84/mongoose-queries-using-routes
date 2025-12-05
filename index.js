4523bcfaa7c731fd73c647012ee8976c9a8a7620
Vasuki t <iamvasukithangaraj@gmail.com>
2025-12-05 21:30:09 +0530

    imported config folder in index.js

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const app = express();
 require("dotenv").config();
+const dbConnection = require("./config/dbConnection.config"); // import config folder
+
+app.use(express.json()); // use middleware
+dbConnection(); // call the imported config folder
 app.listen(process.env.port, () => {
   console.log("8081 server running");
 });
