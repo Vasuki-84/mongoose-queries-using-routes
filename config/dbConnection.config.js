54c66be8e61e3771aed43fc9229f76c7b9f41bb5
Vasuki t <iamvasukithangaraj@gmail.com>
2025-12-06 11:20:27 +0530

    created a connection for atlas

--- config/dbConnection.config.js
+++ config/dbConnection.config.js
@@ -1,24 +1,24 @@
 const mongoose = require("mongoose");
 
-const dbconnection = async () => {
-  try {
-    await mongoose.connect(process.env.dbport);
-    console.log("connection success");
-  } catch (err) {
-    console.log("connection failed");
-  }
-};
-module.exports = dbconnection;
-
-// const dbAtlas = async () => {
+// const dbconnection = async () => {
 //   try {
-//     await mongoose.connect(process.env.dbport, {
-//       ssl: true,
-//       tlsAllowInvalidCertificates: false,
-//     });
-//     console.log("database Atlas connected succcesfully");
+//     await mongoose.connect(process.env.dbport);
+//     console.log("connection success");
 //   } catch (err) {
-//     console.log("database Atlas connection failed");
+//     console.log("connection failed");
 //   }
 // };
-// module.exports= dbconnection;
+// module.exports = dbconnection;
+
+const dbAtlas = async () => {
+  try {
+    await mongoose.connect(process.env.dbPort, {
+      ssl: true,
+      tlsAllowInvalidCertificates: false,
+    });
+    console.log("database Atlas connected succcesfully");
+  } catch (err) {
+    console.log("database Atlas connection failed");
+  }
+};
+module.exports= dbAtlas;
